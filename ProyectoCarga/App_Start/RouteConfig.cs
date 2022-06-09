97c36bae447e2311585e38440bb97b7870dff526
Carlos <carlosporrasayala@gmail.com>
2022-06-08 21:30:11 -0500

    proyecto finalizado

--- ProyectoCarga/App_Start/RouteConfig.cs
+++ ProyectoCarga/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProyectoCarga
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Archivo", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ProyectoCarga/Controllers/ArchivoController.cs
@@ -0,0 +1,82 @@
+﻿using ProyectoCarga.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoCarga.Controllers
+{
+    public class ArchivoController : Controller
+    {
+        static readonly string cadena = "Data Source=(local); Initial Catalog=DB_ARCHIVOS; Integrated Security=true;";
+        static List<Archivos> oLista = new List<Archivos>();
+
+        // GET: Archivo
+        public ActionResult Index()
+        {
+            oLista = new List<Archivos>();
+            using (SqlConnection conn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM ARCHIVOS", conn);
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Archivos archivos = new Archivos {
+                            IdArchivo = Convert.ToInt32(dr["IdArchivo"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Archivo = dr["Archivo"] as byte[],
+                            Extension = dr["Extension"].ToString()
+                        };
+                        oLista.Add(archivos);
+                    }
+                }
+            }
+            return View(oLista);
+        }
+
+        public ActionResult Subir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubirArchivo(string Nombre, HttpPostedFileBase Archivo)
+        {
+            string extension = Path.GetExtension(Archivo.FileName);
+
+            MemoryStream ms = new MemoryStream();
+            Archivo.InputStream.CopyTo(ms);
+            byte[] data = ms.ToArray();
+
+            using (SqlConnection conn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO ARCHIVOS(Nombre, Archivo, Extension) VALUES(@nombre, @archivo, @extension)", conn);
+                cmd.Parameters.AddWithValue("@nombre", Nombre);
+                cmd.Parameters.AddWithValue("@archivo", data);
+                cmd.Parameters.AddWithValue("@extension", extension);
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            return RedirectToAction("Subir", "Archivo");
+        }
+
+        [HttpPost]
+        public FileResult DescargarArchivo(int IdArchivo)
+        {
+            Archivos archivo = oLista.Where(a => a.IdArchivo == IdArchivo).FirstOrDefault();
+            string nombreCompleto = archivo.Nombre + archivo.Extension;
+            
+
+            return File(archivo.Archivo, "application/" + archivo.Extension.Replace(".", ""), nombreCompleto);
+        }
+    }
+}
